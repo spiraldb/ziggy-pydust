a678da3eb27dce63c54d6c7ca9c55a125f9d1bfd
Nicholas Gates <gatesn@users.noreply.github.com>
2023-09-01 18:31:18 +0100

    Initial docs with mkdocs (#9)
    Co-authored-by: Robert Kruszewski <github@robertk.io>

---
+++ example/memory.zig
@@ -0,0 +1,20 @@
+const py = @import("pydust");
+
+// --8<-- [start:append]
+fn append(left: py.PyString, right: []const u8) !void {
+    // Since we create the PyString, and no longer need it after
+    // this function, we are responsible for calling decref on it.
+    const rightPy = try py.PyString.fromSlice(right);
+    defer rightPy.decref();
+
+    _ = try left.append(rightPy);
+}
+// --8<-- [end:append]
+
+pub fn appendFoo(args: *const struct { left: py.PyString }) !void {
+    try append(args.left, "foo");
+}
+
+comptime {
+    py.module(@This());
+}
