8c3fff2523d364f309adad8232e5b7bf91493703
Nicholas Gates <nick@nickgates.com>
2023-09-04 11:39:19 +0100

    Pytest

--- example/memory.zig
+++ example/memory.zig
@@ -1,22 +1,22 @@
 const py = @import("pydust");
 
 // --8<-- [start:append]
-fn append(left: py.PyString, right: []const u8) !void {
+fn append(left: *py.PyString, right: []const u8) !py.PyString {
     const rightPy = try py.PyString.fromSlice(right);
     defer rightPy.decref();
 
-    try left.append(rightPy);
+    return left.append(rightPy);
 }
 // --8<-- [end:append]
 
 // --8<-- [start:append2]
-fn append2(left: py.PyString, right: []const u8) !void {
-    try left.appendSlice(right);
+fn append2(left: *py.PyString, right: []const u8) !py.PyString {
+    return left.appendSlice(right);
 }
 // --8<-- [end:append2]
 
-pub fn appendFoo(args: *const struct { left: py.PyString }) !void {
-    try append(args.left, "foo");
+pub fn appendFoo(args: *const struct { left: py.PyString }) !py.PyString {
+    return append(@constCast(&args.left), "foo");
 }
 
 comptime {
