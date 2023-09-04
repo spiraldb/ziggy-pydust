c57300d2474c71cb8743968e15f7e04d5fd11408
Marko Bakovic <delta003@users.noreply.github.com>
2023-09-04 13:46:33 +0200

    Use pytest.build.zig in debug example (#18)
    Fixes Debug Example

--- pydust/__main__.py
+++ pydust/__main__.py
@@ -7,6 +7,7 @@ sub = parser.add_subparsers(dest="command", required=True)
 
 debug = sub.add_parser("debug", help="Compile a Zig file with debug symbols. Useful for running from an IDE.")
 debug.add_argument("entrypoint")
+debug.add_argument("--buildzig", help="The build.zig file to use. Defaults to 'build.zig'.", default="build.zig")
 
 
 def main():
@@ -19,7 +20,8 @@ def main():
 def debug(args):
     """Given an entrypoint file, compile it for test debugging. Placing it in a well-known location."""
     entrypoint = args.entrypoint
-    buildzig.zig_build(["install", f"-Ddebug-root={entrypoint}"])
+    build_zig_file = args.buildzig
+    buildzig.zig_build(["install", f"-Ddebug-root={entrypoint}"], build_zig=build_zig_file)
 
 
 if __name__ == "__main__":
