94d9529e63f570fc667620be2a91c6b207e110f1
Robert Kruszewski <github@robertk.io>
2023-09-05 15:15:12 +0100

    less

--- pydust/src/modules.zig
+++ pydust/src/modules.zig
@@ -58,7 +58,7 @@ fn Methods(comptime definition: type) type {
                 }
 
                 // The valid types for a "self" parameter are either the module state struct (definition), or a py.PyModule.
-                const sig = funcs.parseSignature(decl.name ++ "", typeInfo.Fn, &.{ py.PyModule, *definition, *const definition });
+                const sig = funcs.parseSignature(decl.name, typeInfo.Fn, &.{ py.PyModule, *definition, *const definition });
                 const def = funcs.wrap(value, sig, getSelfParamFn(sig), 0);
                 defs_ = defs_ ++ .{def};
             }
