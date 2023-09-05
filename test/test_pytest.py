08de3a532209f541c9189cc45bf02ea4f5f2ddd3
Nicholas Gates <nick@nickgates.com>
2023-09-05 10:32:05 +0100

    Boolean Array

--- test/test_pytest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from example import pytest as ex_pytest
-
-
-def test_exceptions():
-    with pytest.raises(ValueError) as exc:
-        ex_pytest.raise_value_error("hello!")
-    assert str(exc.value) == "hello!"
