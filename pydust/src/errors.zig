6a90e96178bdd9f8a3d84565747d55b04167b251
Marko Bakovic <markobakovic95@gmail.com>
2023-09-05 12:03:15 +0200

    union with allocator oom

--- pydust/src/errors.zig
+++ pydust/src/errors.zig
@@ -1,7 +1,8 @@
+const std = @import("std");
+
 pub const PyError = error{
     // Propagate an error raised from another Python function call.
     // This is the equivalent of returning PyNULL and allowing the already set error info to remain.
     Propagate,
-    OOM,
     Raised,
-};
+} || std.mem.Allocator.Error;
