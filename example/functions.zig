01d3a4d223d5b632e1552982337f4192842c2529
Robert Kruszewski <github@robertk.io>
2023-09-07 15:54:24 +0100

    less

--- example/functions.zig
+++ example/functions.zig
@@ -15,13 +15,3 @@ pub fn with_kwargs(args: struct { x: f64, y: f64 = 42.0 }) f64 {
     return if (args.x < args.y) args.x * 2 else args.y;
 }
 // --8<-- [end:kwargs]
-
-// --8<-- [start:exceptions]
-pub fn exceptions(args: struct { x: i64 }) !i64 {
-    if (args.x < 42) {
-        return py.RuntimeError.raise("Provided value is too small");
-    } else {
-        return args.x;
-    }
-}
-// --8<-- [end:exceptions]
