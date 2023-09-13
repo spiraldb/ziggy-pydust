2762fed9867ad762e424a5e358b6c15f5d32afef
Nicholas Gates <nick@nickgates.com>
2023-09-13 11:59:27 +0100

    Buffer changes

--- pydust/src/types.zig
+++ pydust/src/types.zig
@@ -18,6 +18,7 @@ pub usingnamespace @import("types/error.zig");
 pub usingnamespace @import("types/float.zig");
 pub usingnamespace @import("types/list.zig");
 pub usingnamespace @import("types/long.zig");
+pub usingnamespace @import("types/memoryview.zig");
 pub usingnamespace @import("types/module.zig");
 pub usingnamespace @import("types/obj.zig");
 pub usingnamespace @import("types/str.zig");
