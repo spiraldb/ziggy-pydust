ef9a52d9de55c8c07c1ae50dc06bad8d47846cf0
Nicholas Gates <nick@nickgates.com>
2023-09-15 14:34:37 +0200

    Default __new__

--- pydust/src/types/buffer.zig
+++ pydust/src/types/buffer.zig
@@ -101,7 +101,7 @@ pub const PyBuffer = extern struct {
         return @alignCast(std.mem.bytesAsSlice(value_type, self.buf.?[0..@intCast(self.len)]));
     }
 
-    pub fn getFormat(comptime value_type: type) [:0]const u8 {
+    fn getFormat(comptime value_type: type) [:0]const u8 {
         switch (@typeInfo(value_type)) {
             .Int => |i| {
                 switch (i.signedness) {
