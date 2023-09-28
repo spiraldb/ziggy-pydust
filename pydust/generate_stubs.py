1a150550526cc3f360cded42241752f41f992650
Robert Kruszewski <github@robertk.io>
2023-09-28 15:13:39 +0100

    comment

--- pydust/generate_stubs.py
+++ pydust/generate_stubs.py
@@ -1,3 +1,19 @@
+"""
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+# Adapted from https://github.com/huggingface/tokenizers/blob/18bd5e8f9d3aa56612b8aeba5d0d8821e16b3105/bindings/python/stub.py under Apache 2.0 license
+
 import argparse
 import importlib
 import inspect
@@ -8,7 +24,6 @@ from pathlib import Path
 import black
 
 INDENT = " " * 4
-GENERATED_COMMENT = "# Generated content DO NOT EDIT\n"
 
 
 def do_indent(text: str, indent: str):
@@ -34,14 +49,18 @@ def function(obj, indent, text_signature=None):
     string = ""
     string += f"{indent}def {obj.__name__}{text_signature}:\n"
     indent += INDENT
-    if obj.__doc__:
-        string += f'{indent}"""\n'
-        string += f"{indent}{do_indent(obj.__doc__, indent)}\n"
-        string += f'{indent}"""\n'
+    string += doc(obj, indent)
     string += f"{indent}pass\n"
     string += "\n"
     string += "\n"
     return string
+
+
+def doc(obj, indent) -> str:
+    if obj.__doc__:
+        return f'{indent}"""\n{indent}{do_indent(obj.__doc__, indent)}\n{indent}"""\n'
+    else:
+        return ""
 
 
 def member_sort(member):
@@ -55,84 +74,95 @@ def member_sort(member):
 def get_module_members(module):
     members = [
         member
-        for name, member in inspect.getmembers(module)
-        if not name.startswith("_") and not inspect.ismodule(member)
+        for name, member in inspect.getmembers(module, lambda obj: not inspect.ismodule(obj))
+        if not name.startswith("_")
     ]
     members.sort(key=member_sort)
     return members
 
 
-def pyi_file(obj, indent=""):
-    string = ""
+def pyi_file(obj, indent="") -> tuple[str, list[str]]:
+    symbols = []
+    result_content = ""
     if inspect.ismodule(obj):
-        string += GENERATED_COMMENT
+        result_content += doc(obj, indent)
+
+        if indent == "":
+            result_content += f"{indent}from __future__ import annotations\n"
+
         members = get_module_members(obj)
+        members_string = ""
         for member in members:
-            string += pyi_file(member, indent)
+            append, new_symbols = pyi_file(member, indent=indent)
+            members_string += append
+            symbols.extend(new_symbols)
+
+        submodules = inspect.getmembers(module, inspect.ismodule)
+        for name, submodule in submodules:
+            symbols.append(submodule.__name__)
+            members_string += f"{indent}class {submodule.__name__}:\n"
+            submod_indent = indent + INDENT
+            members_string += doc(submodule, submod_indent)
+            submodule_members = get_module_members(submodule)
+            for member in submodule_members:
+                append, new_symbols = pyi_file(member, indent=submod_indent)
+                members_string += append
+
+        if indent == "":
+            result_content += f"{indent}__all__ = {symbols}\n"
+            result_content += members_string
+        else:
+            result_content += members_string
 
     elif inspect.isclass(obj):
-        indent += INDENT
         mro = inspect.getmro(obj)
         if len(mro) > 2:
             inherit = f"({mro[1].__name__})"
         else:
             inherit = ""
-        string += f"class {obj.__name__}{inherit}:\n"
+        result_content += f"{indent}class {obj.__name__}{inherit}:\n"
+        symbols.append(obj.__name__)
+        indent += INDENT
 
-        body = ""
-        if obj.__doc__:
-            body += f'{indent}"""\n{indent}{do_indent(obj.__doc__, indent)}\n{indent}"""\n'
+        class_body = doc(obj, indent)
 
         if obj.__text_signature__:
-            body += f"{indent}def __init__{inspect.signature(obj)}:\n"
-            body += f"{indent+INDENT}pass\n"
-            body += "\n"
-
-        if obj.__annotations__:
-            print("Annotations", obj.__annotations__)
+            class_body += f"{indent}def __init__{inspect.signature(obj)}:\n"
+            class_body += f"{indent+INDENT}pass\n"
+            class_body += "\n"
 
         members = [
             func for name, func in vars(obj).items() if name not in ["__doc__", "__module__", "__new__", "__init__"]
         ]
 
         for member in members:
-            body += pyi_file(member, indent=indent)
+            append, new_symbols = pyi_file(member, indent=indent)
+            class_body += append
+            symbols.extend(new_symbols)
 
-        if not body:
-            body += f"{indent}pass\n"
+        if not class_body:
+            class_body += f"{indent}pass\n"
 
-        string += body
-        string += "\n\n"
+        result_content += class_body
+        result_content += "\n\n"
 
     elif inspect.isbuiltin(obj):
-        string += function(obj, indent)
+        symbols.append(obj.__name__)
+        result_content += function(obj, indent)
 
     elif inspect.ismethoddescriptor(obj):
-        string += function(obj, indent)
+        result_content += function(obj, indent)
 
     elif inspect.isgetsetdescriptor(obj):
-        print("getsetdescr", obj, hasattr(obj, "__set__"), dir(obj.__objclass__))
         # TODO it would be interesing to add the setter maybe ?
-        string += f"{indent}@property\n"
-        string += function(obj, indent, text_signature="(self)")
+        result_content += f"{indent}@property\n"
+        result_content += function(obj, indent, text_signature="(self)")
 
     elif inspect.ismemberdescriptor(obj):
-        string += f"{indent}{obj.__name__}: ..."
+        result_content += f"{indent}{obj.__name__}: ..."
     else:
-        pass
-        # raise Exception(f"Object {obj} is not supported")
-    return string
-
-
-def py_file(module, origin):
-    members = get_module_members(module)
-
-    string = GENERATED_COMMENT
-    string += "\n"
-    for member in members:
-        name = member.__name__
-        string += f"from {origin} import {name} as {name}\n"
-    return string
+        raise Exception(f"Object {obj} is not supported")
+    return result_content, symbols
 
 
 def do_black(content, is_pyi):
@@ -160,24 +190,12 @@ def module_dir(module_name: str) -> Path:
 def write(module, directory, module_name):
     name = simple_name(module_name)
     filename = directory.joinpath(module_dir(module_name)).joinpath(name + ".pyi")
-    pyi_content = pyi_file(module)
+    pyi_content, symbols = pyi_file(module)
     pyi_content = do_black(pyi_content, is_pyi=True)
     os.makedirs(directory, exist_ok=True)
 
     with open(filename, "w") as f:
         f.write(pyi_content)
-
-    # filename = directory.joinpath("__init__.py")
-    # py_content = py_file(module, origin)
-    # py_content = do_black(py_content, is_pyi=False)
-    # os.makedirs(directory, exist_ok=True)
-
-    # with open(filename, "w") as f:
-    #     f.write(py_content)
-
-    # submodules = [(name, member) for name, member in inspect.getmembers(module) if inspect.ismodule(member)]
-    # for name, submodule in submodules:
-    #     write(submodule, directory.joinpath(name), f"{name}")
 
 
 if __name__ == "__main__":
