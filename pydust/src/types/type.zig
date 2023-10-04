24b2b5896e2645b0df2c8359cdf5e21f05f5f34a
Nicholas Gates <nick@nickgates.com>
2023-10-04 15:30:18 +0100

    Refactor call API

--- pydust/src/types/type.zig
+++ pydust/src/types/type.zig
@@ -49,8 +49,10 @@ test "PyType" {
     py.initialize();
     defer py.finalize();
 
-    const StringIO = try PyType.checked(try py.importFrom("io", "StringIO"));
-    defer StringIO.decref();
+    const io = try py.import("io");
+    defer io.decref();
+
+    const StringIO = try io.getAs(py.PyType, "StringIO");
     try std.testing.expectEqualSlices(u8, "StringIO", try (try StringIO.name()).asSlice());
 
     const sio = try py.call0(py.PyObject, StringIO);
