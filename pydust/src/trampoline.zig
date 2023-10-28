d40211c0c38284f54a4f73a2d0461ba2aec51247
Nicholas Gates <nick@nickgates.com>
2023-10-28 22:27:02 +0100

    Remove isinstance check when converting Python object to Pydust class

--- pydust/src/trampoline.zig
+++ pydust/src/trampoline.zig
@@ -223,20 +223,7 @@ pub fn Trampoline(comptime T: type) type {
 
                         // If the pointer is for a Pydust class
                         if (def.type == .class) {
-                            // TODO(ngates): #193
-                            const Cls = try py.self(p.child);
-                            defer Cls.decref();
-
-                            if (!try py.isinstance(obj, Cls)) {
-                                const clsName = State.getIdentifier(p.child).name;
-                                const mod = State.getContaining(p.child, .module);
-                                const modName = State.getIdentifier(mod).name;
-                                return py.TypeError.raiseFmt(
-                                    "Expected {s}.{s} but found {s}",
-                                    .{ modName, clsName, try obj.getTypeName() },
-                                );
-                            }
-
+                            // Note(ngates): we don't perform an isinstance check since it's potentially expensive.
                             const PyType = pytypes.PyTypeStruct(p.child);
                             const pyobject = @as(*PyType, @ptrCast(obj.py));
                             return @constCast(&pyobject.state);
