497fb3dfbfc95767bd0a90f288866b7c8f7f73ed
jorenham <jhammudoglu@gmail.com>
2025-04-30 20:49:52 +0200

    regenerate stubs

--- example/classes.pyi
+++ example/classes.pyi
@@ -1,49 +1,34 @@
-from __future__ import annotations
-
 class Animal:
     def species(self, /): ...
 
 class Callable:
-    def __init__(self, /):
-        pass
-
+    def __init__(self, /) -> None: ...
     def __call__(self, /, *args, **kwargs):
         """
         Call self as a function.
         """
-        ...
 
 class ConstructableClass:
-    def __init__(self, count, /):
-        pass
+    def __init__(self, count, /) -> None: ...
 
 class Counter:
-    def __init__(self, /):
-        pass
-
+    def __init__(self, /) -> None: ...
     def increment(self, /): ...
-
     count: ...
 
 class GetAttr:
-    def __init__(self, /):
-        pass
-
+    def __init__(self, /) -> None: ...
     def __getattribute__(self, name, /):
         """
         Return getattr(self, name).
         """
-        ...
 
 class Hash:
-    def __init__(self, x, /):
-        pass
-
-    def __hash__(self, /):
+    def __init__(self, x, /) -> None: ...
+    def __hash__(self, /) -> int:
         """
         Return hash(self).
         """
-        ...
 
 class Math:
     def add(x, y, /): ...
@@ -54,22 +39,16 @@ class SomeClass:
     """
 
 class User:
-    def __init__(self, name, /):
-        pass
-
+    def __init__(self, name, /) -> None: ...
     @property
-    def email(self): ...
+    def email(self, /): ...
     @property
-    def greeting(self): ...
+    def greeting(self, /): ...
 
 class ZigOnlyMethod:
-    def __init__(self, x, /):
-        pass
-
+    def __init__(self, x, /) -> None: ...
     def reexposed(self, /): ...
 
 class Dog(Animal):
-    def __init__(self, breed, /):
-        pass
-
+    def __init__(self, breed, /) -> None: ...
     def breed(self, /): ...
